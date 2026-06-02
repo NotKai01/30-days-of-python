5fa8c06870602d4d7f418e50915311e899b94f53
NotKai01 <kaichinaleong@gmail.com>
2026-06-02 17:03:27 +0000

    Day 1 Completed

---
+++ day_01.py
@@ -0,0 +1,36 @@
+2+3 # addition
+2-3 # subtraction
+2*3 # multiplication
+2/3 # division
+2**3 # exponential
+2//3 # removes remainder
+2%3 # modulus 
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+print('Kai')
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Kai'))
+
